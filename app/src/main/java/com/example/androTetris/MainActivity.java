fdde50e202584b719f880fe101644b8f352df58a
Kamil Michna <kamil.michna28@gmail.com>
2023-12-18 19:08:54 +0100

    initial

---
+++ app/src/main/java/com/example/androTetris/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androTetris;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
